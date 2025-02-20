24b6c9d2ae5394daf65f664049936bd880403794
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-02-20 17:11:48 +0900

    (2025.02.20) 움직임 로직

---
+++ Undead Survivor/Assets/Code/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+
+    Rigidbody2D rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+
+        // GetAxis'Raw'("") rawÃß°¡·Î °ª ±ò²ûÇÏ°Ô ¹Ý¿µ
+    }
+
+    private void FixedUpdate()
+    {
+        //// Èû
+        //rigid.AddForce (inputVec);
+
+        //// ¼Óµµ
+        //rigid.velocity = inputVec;
+
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition (rigid.position + nextVec);
+    }
+}
