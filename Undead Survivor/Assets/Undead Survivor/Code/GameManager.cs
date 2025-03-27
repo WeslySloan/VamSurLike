51d0d69dc863575d8baa3848a6d75c2b53c0637a
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-03-28 04:16:50 +0900

    (2025.03.28) 2,3번캐릭터 무기 미지급 오류 수정

--- Undead Survivor/Assets/Undead Survivor/Code/GameManager.cs
+++ Undead Survivor/Assets/Undead Survivor/Code/GameManager.cs
@@ -38,7 +38,7 @@ public class GameManager : MonoBehaviour
         health = maxHealth;
 
         player.gameObject.SetActive(true);
-        uiLevelUp.Select(playerId);  // 임시 스크립트 (첫번쨰 캐릭터 선택)
+        uiLevelUp.Select(playerId % 2);  // 홀수 번째 근접무기, 짝수번째 원거리무기
         Resume();
     }
 
