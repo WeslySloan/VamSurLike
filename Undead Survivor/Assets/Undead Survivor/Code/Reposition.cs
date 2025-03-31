011bc6b8b213c31569fee1ebfbd401eb9fc6c7a5
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-04-01 03:13:54 +0900

    (2025.04.01) 재배치 로직 수정

--- Undead Survivor/Assets/Undead Survivor/Code/Reposition.cs
+++ Undead Survivor/Assets/Undead Survivor/Code/Reposition.cs
@@ -17,20 +17,26 @@ public class Reposition : MonoBehaviour
             return;
         Vector3 playerPos = GameManager.instance.player.transform.position;
         Vector3 myPos = transform.position;
-        float dirX = playerPos.x - myPos.x;
-        float dirY = playerPos.y - myPos.y;
-        float diffx = Mathf.Abs(dirX);
-        float diffy = Mathf.Abs(dirY);
 
+        //float dirX = playerPos.x - myPos.x;
+        //float dirY = playerPos.y - myPos.y;
+        //float diffx = Mathf.Abs(dirX);
+        //float diffy = Mathf.Abs(dirY);
 
+        //dirX = dirX > 0 ? 1 : -1;
+        //dirY = dirY > 0 ? 1 : -1;
 
-
-
-        dirX = dirX > 0 ? 1 : -1;
-        dirY = dirY > 0 ? 1 : -1;
         switch (transform.tag)
         {
             case "Ground":
+                float dirX = playerPos.x - myPos.x;
+                float dirY = playerPos.y - myPos.y;
+                float diffx = Mathf.Abs(dirX);
+                float diffy = Mathf.Abs(dirY);
+
+                dirX = dirX > 0 ? 1 : -1;
+                dirY = dirY > 0 ? 1 : -1;
+
                 if (diffx > diffy)
                 {
                     transform.Translate(Vector3.right * dirX * 40);
